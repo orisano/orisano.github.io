2c00429b66ba4d9c972c182384b510ba6cc148a3
Orisano <weekendsummer.endless@gmail.com>
2014-01-13 18:01:54 +0900

    can draw graph

--- js/main.js
+++ js/main.js
@@ -1,17 +1,46 @@
-$(function(){
-	var apiUrl = "http://judge.u-aizu.ac.jp/onlinejudge/webservice/user?id=";
-	var users = ["raimei10130", "kagamiz", "orisano"];
-	var userTemplate = _.template("<tr><td><%= name %></td><td><%= solved %></td></tr>");
-	for (var i = 0; i < users.length; ++i){
+$(function(){	
+	var getSolved = function(username, callback){
+		var apiUrl = "http://judge.u-aizu.ac.jp/onlinejudge/webservice/user?id=";
 		$.ajax({
-			url: apiUrl + users[i],
+			url: apiUrl + username,
 			type: 'get',
 			dataType: 'xml',
 			success: function(xml, status){
-				var username = $(xml).find("user > id").text();
-				var solved = $(xml).find("user > status > solved").text();
-				$("#watch-table").append(userTemplate({name: username, solved: solved}));
+				var username_ = $(xml).find("user > id").text();
+				var solved_ = $(xml).find("user > status > solved").text();
+				callback(username_, solved_, status);
 			}
 		});
 	}
+
+	var users = ["raimei10130", "kagamiz", "orisano", "li_saku", "marin72_com"];
+	var users_ = [];
+	var solved_ = [];
+	var count = 0;
+	var userTemplate = _.template("<tr><td><%= name %></td><td><%= solved %></td></tr>");
+	_.each(users, function(user){ getSolved(user, function(username, solved){
+		$("table#watch-table").append(userTemplate({name: username, solved: solved}));
+		count++;
+		users_.push(username);
+		solved_.push(solved);
+	});});
+
+	var waitComplete = function(span, callback){
+		if (count == users.length){
+			callback();
+		}
+		else {
+			setTimeout(function(){
+				waitComplete(span, callback);
+			}, span);
+		}
+	};
+	
+	waitComplete(1000, function(){
+		var ctx = document.getElementById("solved-graph").getContext("2d");
+		new Chart(ctx).Bar({
+			labels: users_,
+			datasets : [{ data : solved_ }]
+		});
+	});
 });
